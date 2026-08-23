0772b58b86e4f8e63d3f1ae87585fd54b1e41458
Syeda Samana Batool Abedi <syedasamanaabedi@gmail.com>
2026-08-24 02:32:18 +0500

    Update frontend to use live backend URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,12 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: "https://event-registration-backend-b1a2.onrender.com/api",
 });
-
 // Automatically attach token to every request if logged in
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
